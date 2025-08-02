2d4a02b307f0f88e7b5e37c4851688e716d9cf8e
sabenih726 <sabenih726@gmail.com>
2025-08-03 01:37:10 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'UAS Traffic Control',
+  description: 'Created with RizkiFerm',
 }
 
 export default function RootLayout({
